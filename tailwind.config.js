ec5b9dd0e5f61d1bed9c65b4ba9a22b513d92aa0
Brian <desiredshellstate@gmail.com>
2022-05-14 20:17:44 +0200

    Added some Tailwind Styles and Playing Around with it

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,27 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+      "2xl": "1600px",
+      "3xl": "2400px",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      purple: "#3f3cbb",
+      midnight: "#121063",
+      metal: "#565584",
+      tahiti: "#3ab7bf",
+      silver: "#ecebff",
+      "bubble-gum": "#ff77e9",
+      bermuda: "#78dcca",
+      blue: "#1E40AF",
+    },
     extend: {},
   },
   plugins: [],
